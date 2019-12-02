3508f6a7f33f541bf0802613f495eddc9833e4ac
shuennhauc <54190149+shuennhauc@users.noreply.github.com>
2019-12-02 12:51:42 -0500

    Update Age Bars
    Update Bars

--- bars.js
+++ bars.js
@@ -1,32 +1,30 @@
-var ppl=[];
-
 var svg2= d3.select("#svg2")
 
- var MAX_BAR_HEIGHT = 200;
+ var MAX_BAR_HEIGHT_AGE = 200;
 
  BAR_WIDTH=30;
 var textcenter=BAR_WIDTH/2;
 
- var yaxis_adjust=MAX_BAR_HEIGHT-140;
- var genbar_start= 0;
- var max_bar_value=150
+ var yaxis_adjust_age=MAX_BAR_HEIGHT_AGE-140;
+ var agebar_start= 0;
+ var max_bar_value_age=80
 
- var bar_yScale = d3.scale.linear()
-            .domain([0,max_bar_value])
-            .range([MAX_BAR_HEIGHT, 0]);
+ var bar_age_yScale = d3.scale.linear()
+            .domain([0,max_bar_value_age])
+            .range([MAX_BAR_HEIGHT_AGE, 0]);
 
- var bar_hScale = d3.scale.linear()
-             .domain([0, max_bar_value])
-             .range([0, MAX_BAR_HEIGHT]);
+ var bar_age_hScale = d3.scale.linear()
+             .domain([0, max_bar_value_age])
+             .range([0, MAX_BAR_HEIGHT_AGE]);
 
  var yAxis_age = d3.svg.axis()
-           .scale(bar_yScale)
+           .scale(bar_age_yScale)
            .orient("left")
            .ticks(4);
 
    svg2.append("g")
         .attr("class", "age axis")
-        .attr("transform", "translate(130,"+yaxis_adjust+")")
+        .attr("transform", "translate(130,"+yaxis_adjust_age+")")
         .call(yAxis_age);
 //Age X axis
  var age_xScale = d3.scale.ordinal()
@@ -63,19 +61,46 @@ var textcenter=BAR_WIDTH/2;
      .attr("stroke","black")
      .attr("fill", "gold")
      .attr('height',function(d){
-           return bar_hScale(d3.sum(ppl, function(d) {
-                             return d[2]==agegroup;
+           return bar_age_hScale(d3.sum(ppl, function(d) {
+                             return d.age==agegroup;
                          })
                        );
                      })
      .attr('width', function(d){return BAR_WIDTH;})
      .attr("x", x_value)
      .attr("y", function(d){
-           return genbar_start + bar_yScale(d3.sum(ppl, function(d) {
-                               return d[2]==agegroup;
+           return agebar_start + bar_age_yScale(d3.sum(ppl, function(d) {
+                               return d.age==agegroup;
                            })
                          );
                      })
+     .on("click",function(data){
+        if (!d3.select(this).classed("selected")) {
+          d3.select(this).classed("selected", true)
+          d3.select(this).transition().attr("fill","lightgray")
+
+          //update map
+          d3.selectAll("circle")
+            .transition()
+            .duration(5)
+            .filter(function(d){return d.Age==agegroup})
+              //.style("fill", "lightblue")
+              .attr("r",0);
+
+        }
+        else{
+          d3.select(this).classed("selected", false);
+          d3.select(this).transition().attr("fill","gold");
+
+          //update map
+          d3.selectAll("circle")
+            .transition()
+            .duration(5)
+            .filter(function(d){return d.Age==agegroup})
+              //.style("fill", "red")
+              .attr("r",3)
+              }
+          })
 
        //txt agegroup total
        svg2.select(id).selectAll("text")
@@ -84,21 +109,22 @@ var textcenter=BAR_WIDTH/2;
           .append("text")
           .text(
                  d3.sum(ppl, function(d) {
-                     return d[2]==agegroup;
+                     return d.age==agegroup;
 
               })
             )
           .attr("text-anchor", "middle")
           .attr("x", (x_value+textcenter))
           .attr("y", function(d){
-                        return (genbar_start-5) + bar_yScale(d3.sum(ppl, function(d) {
-                            return d[2]==agegroup;
+                        return (agebar_start-5) + bar_age_yScale(d3.sum(ppl, function(d) {
+                            return d.age==agegroup;
                           })
                         );
                       })
           .attr("font-family", "sans-serif")
           .attr("font-size", "11px")
           .attr("fill", "blue");
+
 
    }
 
@@ -107,25 +133,25 @@ function AgeChart()
 {
   start=100
   space=BAR_WIDTH+5
-  infoage("#agechart0","<1 Year",start);
+  infoage("#agechart0","<1Year",start);
 
-  infoage("#agechart1","1-4 Years",start + space);
+  infoage("#agechart1","1-4Years",start + space);
 
-  infoage("#agechart2","5-14 Years",start + 2*space);
+  infoage("#agechart2","5-14Years",start + 2*space);
 
-  infoage("#agechart3","15-24 Years",start + 3*space);
+  infoage("#agechart3","15-24Years",start + 3*space);
 
-  infoage("#agechart4","25-34 Years",start + 4*space,);
+  infoage("#agechart4","25-34Years",start + 4*space,);
 
-  infoage("#agechart5","35-44 Years",start + 5*space,);
+  infoage("#agechart5","35-44Years",start + 5*space,);
 
-  infoage("#agechart6","45-54 Years",start + 6*space,);
+  infoage("#agechart6","45-54Years",start + 6*space,);
 
-  infoage("#agechart7","55-64 Years",start + 7*space,);
+  infoage("#agechart7","55-64Years",start + 7*space,);
 
-  infoage("#agechart8","65-74 Years",start + 8*space,);
+  infoage("#agechart8","65-74Years",start + 8*space,);
 
-  infoage("#agechart9","75-84 Years",start + 9*space,);
+  infoage("#agechart9","75-84Years",start + 9*space,);
 
-  infoage("#agechart10",">=85 Years",start + 10*space);
+  infoage("#agechart10",">=85Years",start + 10*space);
 }
